46a0db526ee0ea9f23e2509e0f399639c3682462
Lucas Sulzbach rilho <lsr.jupi@gmail.com>
2023-04-12 12:48:43 +0200

    ex02

---
+++ ex02/whois.py
@@ -0,0 +1,18 @@
+import sys
+
+try:
+    assert len(sys.argv) <= 2, "More than one argument provided"
+    assert len(sys.argv) > 1, "Usage: python whois.py NUMBER"
+    try:
+        num = int(sys.argv[1])
+    except:
+        raise AssertionError("Argument is not an integer")
+except AssertionError as msg:
+    print("AssertionError:", msg)
+else:
+    if num == 0:
+        print("I am Zero!");
+    elif num % 2 == 0:
+        print("I am Even!");
+    else:
+        print("I am Odd!");
